53cc4dbf581f3d92233b7d055a25d4b25a73f88f
Yuqing Wei <12311043@mail.sustech.edu.cn>
2026-08-14 15:31:19 +0800

    Fix vite config and simplify workflow

--- project2/my-vue-app（frontend）/vite.config.js
+++ project2/my-vue-app（frontend）/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/Database_principle/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: {
     port: 8080,
     proxy: {
